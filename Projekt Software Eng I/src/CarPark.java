f9206c40824925fd4c796d32f458c858d9440d80
lennart <lennart@lennarts-mbp.fritz.box>
2018-01-07 16:31:12 +0100

    BLUBB

---
+++ Projekt Software Eng I/src/CarPark.java
@@ -0,0 +1,23 @@
+
+public interface CarPark {
+	/**
+	 * 
+	 * @return amount of free parking slots in car park
+	 */
+	public ParkingSlot[] getFreeParkingSlots();
+	/**
+	 * 
+	 * @return amount of occupied parking slots in car park
+	 */
+	public ParkingSlot[] getOccupiedParkingSlots();
+	/**
+	 * 
+	 * @return
+	 */
+	public ParkingSlot[] getParkingSlots();
+	/**
+	 * 
+	 * @return success
+	 */
+	public boolean setParkingSlots(ParkingSlot[] ps1);
+}
---
+++ Projekt Software Eng I/src/CarParkTest.java
@@ -0,0 +1,35 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarParkTest {
+	
+	CarPark cp = null; //Not implemented yet
+	ParkingSlot[] ps1 = null; //Not implemented yet
+
+	@Test
+	public void getFreeParkingSlots_SimpleCall_NotNull() {
+		assertNotNull(cp.getFreeParkingSlots());
+	}
+	
+	@Test
+	public void getOccupiedParkingSlots_SimpleCall_NotNull() {
+		assertNotNull(cp.getOccupiedParkingSlots());
+	}
+
+	@Test
+	public void getParkingSlots_SimpleCall_NotNull() {
+		assertNotNull(cp.getParkingSlots());
+	}
+
+	@Test
+	public void setParkingSlots_SimpleCall_SuccessTrue() {
+		assertTrue(cp.setParkingSlots(ps1));
+	}
+	
+	@Test
+	public void setParkingSlots_SimpleCall_SuccessFalse() {
+		assertTrue(cp.setParkingSlots(null));
+	}
+}
